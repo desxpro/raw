a7d6cc33ab6c99b3abbe1c3e8770e404a5366876
desxpro <desxpro@gmail.com>
2024-11-27 13:04:17 +0200

    homeWork1

--- js.js
+++ js.js
@@ -1,66 +1,125 @@
 // создает масив от и до
-const range = (start,end,step=null) =>{             
-    let m = []
-    for(let i = start; i <= end; i++) {
-        if(step !== null){
-            if(step) i%step ? m.push(i) : null
-            if(step<0) m.unshift(i)
-        }else m.push(i)
-    }
-    return m
-}
+const range = (start, end, step = null) => {
+  let m = [];
+  for (let i = start; i <= end; i++) {
+    if (step !== null) {
+      if (step) i % step ? m.push(i) : null;
+      if (step < 0) m.unshift(i);
+    } else m.push(i);
+  }
+  return m;
+};
 
-// возвращает сумму масива 
-const sum = (array) => array.reduce((a,e) => a+e, 0) 
+// возвращает сумму масива
+const sum = (array) => array.reduce((a, e) => a + e, 0);
 
 // масив в список
-const arrayToList = (array) => array.length != 0 ? {value:array[0], rest:arrayToList(array.slice(1)) || null}:null
+const arrayToList = (array) =>
+  array.length != 0
+    ? { value: array[0], rest: arrayToList(array.slice(1)) || null }
+    : null;
 
 //список в масив
-const listToArray = (list) => list ? [list.value].concat(listToArray(list.rest)): []
-   
- //console.log(arrayToList([10,20,30]))
+const listToArray = (list) =>
+  list ? [list.value].concat(listToArray(list.rest)) : [];
 
-const prepend = (element,list) => ({value:element, rest:list})
+//console.log(arrayToList([10,20,30]))
+
+const prepend = (element, list) => ({ value: element, rest: list });
 
 //console.log(prepend(10, arrayToList([20,30,40])));
 
-const nth = (list,index) =>listToArray(list)[index]
+const nth = (list, index) => listToArray(list)[index];
 
 //console.log(nth(arrayToList([10,20,30]),1))
 
-
 // глибоке порiвняння обьектов
-const deepEqual = (a,b) =>{
-    if(a !== null && b !== null){       // решаем проблему с null == object
-        if(typeof a == 'object' && typeof b == 'object')
-        {
-            let keyA = Object.keys(a)
-            let keyB = Object.keys(b)
-            if(keyA.length !== keyB.length) return false
-            
-            for(let key of keyA){
-                if (!deepEqual(a[key], b[key])) return false
-            }
-        }else return a == b
-    }
-    return true
-}
+const deepEqual = (a, b) => {
+  if (a !== null && b !== null) {
+    // решаем проблему с null == object
+    if (typeof a == "object" && typeof b == "object") {
+      let keyA = Object.keys(a);
+      let keyB = Object.keys(b);
+      if (keyA.length !== keyB.length) return false;
 
+      for (let key of keyA) {
+        if (!deepEqual(a[key], b[key])) return false;
+      }
+    } else return a == b;
+  }
+  return true;
+};
 
-let o1 ={
-    a:{e:{p:{a1:1}},d:{a2:2}},
-    b:{j:{z:4,x:3},h:{u:9}},
-    c:{t:{n:34,v:{r:45}},f:{io:{d:3,yu:4}}},
-    d:{s:{k:3},y:{i:{i2:22,y:{t:{tt:99}}}}}
-}
+let o1 = {
+  a: { e: { p: { a1: 1 } }, d: { a2: 2 } },
+  b: { j: { z: 4, x: 3 }, h: { u: 9 } },
+  c: { t: { n: 34, v: { r: 45 } }, f: { io: { d: 3, yu: 4 } } },
+  d: { s: { k: 3 }, y: { i: { i2: 22, y: { t: { tt: 99 } } } } },
+};
 
-let o2 ={
-    a:{e:{p:{a1:1}},d:{a2:2}},
-    b:{j:{z:4,x:3},h:{u:9}},
-    c:{t:{n:34,v:{r:45}},f:{io:{d:3,yu:4}}},
-    d:{s:{k:3},y:{i:{i2:22,y:{t:{tt:99}}}}}
-}
+let o2 = {
+  a: { e: { p: { a1: 1 } }, d: { a2: 2 } },
+  b: { j: { z: 4, x: 3 }, h: { u: 9 } },
+  c: { t: { n: 34, v: { r: 45 } }, f: { io: { d: 3, yu: 4 } } },
+  d: { s: { k: 3 }, y: { i: { i2: 22, y: { t: { tt: 99 } } } } },
+};
 
+console.log(deepEqual(o1, o2));
 
-console.log(deepEqual(o1,o2))
+const summ = (number) => {
+  return function (x) {
+    return number * x;
+  };
+};
+const summ2 = summ(3);
+console.log(summ2(3));
+
+const createMultiplier = (n) => (m) => n * m;
+
+const multiplyBy2 = createMultiplier(2);
+
+//console.log(multiplyBy2(100));
+
+let m = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const inBetween = (a, b) => (x) => x >= a && x <= b;
+const inArray = (array) => (x) => array.includes(x);
+
+//console.log(m.filter(inArray([3, 5, 10])));
+
+const student = [
+  { name: "Максим", age: 17, surname: "Іваничук" },
+  { name: "Артем", age: 19, surname: "Писаренко" },
+  { name: "Петро", age: 18, surname: "Петренко" },
+];
+
+const byField = (x) => (a, b) => a[x] > b[x] ? 1 : -1;
+
+// console.log(student.sort(byField("age")));
+
+const createLimitedFunction = (fn, limit) => (x) => x <= limit ? fn(x) : null;
+
+const limitedFn = createLimitedFunction((x) => console.log(2 * x), 2);
+
+// limitedFn(1);
+
+const createSecret = () => {
+  let secret = null;
+  return {
+    getSecter() {
+      return secret;
+    },
+    setSecret(value) {
+      secret = value;
+    },
+  };
+};
+
+// const secret = createSecret();
+// secret.setSecret("My secret");
+
+// console.log(secret.getSecter());
+
+const multiply = (a) => (b) => !b ? a : multiply(a * b);
+
+console.log(multiply(5)(5)());
